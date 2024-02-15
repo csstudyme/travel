7f0ee70a86a831be3affa9af71c995ab213fd8ed
Ajay <ajaypanpatil01@gmail.com>
2024-02-15 17:20:01 +0530

    Add project to git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './app.css'
 
-function App() {
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Main from './Components/Main/Main'
+import Footer from './Components/Footer/Footer'
+import LoginSignup from './Components/LoginSignup/LoginSignup'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    <Navbar/>
+    <Home/>
+    {/* <Main/>
+    <Footer/>
+    <LoginSignup /> */}
+    </>
+  )
 }
 
-export default App;
+export default App
