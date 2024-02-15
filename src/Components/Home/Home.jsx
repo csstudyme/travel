f234c299a6ed83f31434162f7a6669e6049ecc4b
Ajay <ajaypanpatil01@gmail.com>
2024-02-15 18:16:39 +0530

    Home design

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,6 +4,13 @@ import './home.css'
 import video from "../../Assets/video.mp4";
 import {GrLocation} from 'react-icons/gr'
 import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { BsInstagram } from "react-icons/bs";
+import { FaTripadvisor } from "react-icons/fa";
+import { BsListTask } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
+
+
 
 const Home = () => {
   return (
@@ -53,6 +60,19 @@ const Home = () => {
           <span>More Filter</span>
           </div>
         </div>
+
+        <div className="homeFooterIcons flex">
+          <div className="rightIcons">
+          <FiFacebook className="icon" />
+          <BsInstagram className="icon" />
+          <FaTripadvisor className="icon" />
+          </div>
+          <div className="leftIcons">
+          <BsListTask className="icon" />
+          <TbApps className="icon" />
+          </div>
+        </div>
+
       </div>
     </section>
   );
